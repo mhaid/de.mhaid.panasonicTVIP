806358265134ec758176ee93667f82036189b4ff
Morris Haid <44113328+mhaid@users.noreply.github.com>
2021-02-12 12:15:39 +0100

    Fixed bug

--- drivers/panasonic_viera/device.js
+++ drivers/panasonic_viera/device.js
@@ -25,7 +25,7 @@ class VieraDevice extends Homey.Device {
 		this.registerCapabilityListener('tv_selector_lor', this.onCapabilityTvSelectorLor.bind(this));
 		
 		// Check for Status every 5 Minutes
-		this.homey.setInterval(await this.checkOnOff(this),300000);
+		this.homey.setInterval(this.checkOnOff(this),300000);
 	}
 
 	/*onDiscoveryResult(discoveryResult) {
